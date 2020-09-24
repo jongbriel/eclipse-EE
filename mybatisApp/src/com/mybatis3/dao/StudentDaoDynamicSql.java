411a4611c59abee463708f4038e83b377dbe955a
jongbriel <Jongbriel90@gmail.com>
2020-09-24 12:53:10 +0900

    home

--- mybatisApp/src/com/mybatis3/dao/StudentDaoDynamicSql.java
+++ mybatisApp/src/com/mybatis3/dao/StudentDaoDynamicSql.java
@@ -27,11 +27,27 @@ public class StudentDaoDynamicSql {
 	}
 	public List<Student> findStudents(Student findStudent){
 		SqlSession sqlSession=sqlSessionFactory.openSession();
-		List<Student> studentList=sqlSession.selectList(NAMESPACE+"findStudents", findStudent);
+		List<Student> studentList=
+				sqlSession.selectList(NAMESPACE+"findStudents",findStudent);
 		sqlSession.close();
 		return studentList;
-		
 	}
+	public List<Student> findStudentsOrder(String columnName){
+		SqlSession sqlSession=sqlSessionFactory.openSession();
+		List<Student> studentList=
+				sqlSession.selectList(NAMESPACE+"findStudentsOrder",columnName);
+		sqlSession.close();
+		return studentList;
+	}
+	public int updateStudentById(Student updateStudent) {
+		SqlSession sqlSession=sqlSessionFactory.openSession(false);
+		int updateRowCount=
+				sqlSession.update(NAMESPACE+"updateStudentById", updateStudent);
+		sqlSession.commit();
+		sqlSession.close();
+		return updateRowCount;
+	}
+	
 	
 }
 
